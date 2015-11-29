37ae0efc3d8f5f38a06d4c8acd309e368da9f1fd
Joe Bruckner <joembruckner@gmail.com>
2015-11-29 09:08:24 -0500

    Touch up the cast text

--- app/src/main/java/joebruckner/lastpick/data/Credits.kt
+++ app/src/main/java/joebruckner/lastpick/data/Credits.kt
@@ -7,7 +7,7 @@ data class Credits(
     override fun toString(): String {
         val str = StringBuilder()
         val size = if (cast.size > 3) 3 else cast.size
-        for (i in 0..size-1) str.append(cast[i].name + "  ")
-        return str.toString()
+        for (i in 0..size-1) str.append(", ${cast[i].name}")
+        return str.toString().substring(2)
     }
 }
