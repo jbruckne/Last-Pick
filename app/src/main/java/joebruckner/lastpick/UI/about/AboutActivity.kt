dcf943afc9dc52a76652b6cd39068132ad19b24c
Joe Bruckner <joembruckner@gmail.com>
2015-12-02 20:14:20 -0500

    Add new launcher icon

---
+++ app/src/main/java/joebruckner/lastpick/UI/about/AboutActivity.kt
@@ -0,0 +1,15 @@
+package joebruckner.lastpick.ui.about
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import joebruckner.lastpick.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+    }
+}
